6b9c9d0086aa94489e31e53c8e8ba6562900bcb5
webdev1014 <aleksagegicweb@gmail.com>
2020-08-12 06:36:35 +0300

    update celery script

--- run_celery.sh
+++ run_celery.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-celery -A inventory_source_scraper.application.celery worker --logfile="logs/error.log" -l ERROR
+celery -A inventory_source_scraper.application.celery worker --logfile="logs/error.log" -l ERROR --concurrency=8
