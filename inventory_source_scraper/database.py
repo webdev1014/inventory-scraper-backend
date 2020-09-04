9fc65f725537a7e5fcb1869eebf175ab3f77bd5e
webdev1014 <aleksagegicweb@gmail.com>
2020-09-04 05:14:03 +0300

    reconnect when mysql server has gone away

--- inventory_source_scraper/database.py
+++ inventory_source_scraper/database.py
@@ -29,9 +29,16 @@ class Database:
         self.cur.execute('set global max_allowed_packet=134217728')
 
     def remove_data(self):
+        if not self.con.open:
+            self.con.ping(reconnect=True)
+        self.cur = self.con.cursor()
         self.cur.execute('TRUNCATE TABLE `products`')
 
     def save_data(self, product):
+        if not self.con.open:
+            self.con.ping(reconnect=True)
+        self.cur = self.con.cursor()
+
         query = """INSERT INTO `products` (`name`, `upc`, `vendor`, `company`, `price_inventory`, `price_msrp`, 
         `price_amazon`, `shipping_amazon`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s) """
         self.cur.execute(query, (product['name'],
@@ -45,12 +52,20 @@ class Database:
         return
 
     def get_num_rows(self):
+        if not self.con.open:
+            self.con.ping(reconnect=True)
+        self.cur = self.con.cursor()
+
         query = 'SELECT COUNT(*) FROM `products`'
         self.cur.execute(query)
         result = self.cur.fetchall()
         return result[0]['COUNT(*)']
 
     def get_products(self, start_at, batch_size):
+        if not self.con.open:
+            self.con.ping(reconnect=True)
+        self.cur = self.con.cursor()
+        
         query = 'SELECT * FROM `products` ORDER BY `name` DESC LIMIT %i, %i'
         query = query % (start_at * batch_size, batch_size)
         self.cur.execute(query)
